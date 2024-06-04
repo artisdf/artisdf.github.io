f5bb8df85c96433d75a6087af8780e92a1aa5496
artisdf <artisdf18@gmail.com>
2024-06-05 00:54:27 +0300

    Updated path.html

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const navLinks = document.querySelectorAll(".nav-link");
-    const themeSwitch = document.querySelector(".theme-switch");
-    const logo = document.querySelector(".logo");
-    const mainLogo = document.getElementById("main-logo");
-    const themeIcon = document.getElementById("theme-icon");
-    const body = document.body;
-  
-    navLinks.forEach((link) => {
-      link.addEventListener("click", () => {
-        navLinks.forEach((link) => link.classList.remove("active"));
-        link.classList.add("active");
-      });
-    });
-  
-    themeSwitch.addEventListener("click", () => {
-      body.classList.toggle("dark-mode");
-      if (body.classList.contains("dark-mode")) {
-        themeIcon.src = "images/dark mode.png";
-        logo.src = "images/melns logo.png";
-        mainLogo.src = "images/main_logo_melns.png";
-      } else {
-        themeIcon.src = "images/light mode.png";
-        logo.src = "images/balts logo.png";
-        mainLogo.src = "images/main_logo_balts.png";
-      }
+  const navLinks = document.querySelectorAll(".nav-link");
+  const themeSwitch = document.querySelector(".theme-switch");
+  const logo = document.querySelector(".logo");
+  const mainLogo = document.getElementById("main-logo");
+  const themeIcon = document.getElementById("theme-icon");
+  const body = document.body;
+
+  // Function to toggle dark mode
+  const toggleDarkMode = () => {
+    body.classList.toggle("dark-mode");
+    if (body.classList.contains("dark-mode")) {
+      themeIcon.src = "images/dark mode.png";
+      logo.src = "images/melns logo.png";
+      mainLogo.src = "images/main_logo_melns.png";
+      // Save dark mode state to Session Storage
+      sessionStorage.setItem("darkMode", "true");
+    } else {
+      themeIcon.src = "images/light mode.png";
+      logo.src = "images/balts logo.png";
+      mainLogo.src = "images/main_logo_balts.png";
+      // Remove dark mode state from Session Storage
+      sessionStorage.removeItem("darkMode");
+    }
+  };
+
+  // Check if dark mode is enabled in Session Storage
+  const isDarkModeEnabled = sessionStorage.getItem("darkMode") === "true";
+  if (isDarkModeEnabled) {
+    body.classList.add("dark-mode");
+    themeIcon.src = "images/dark mode.png";
+    logo.src = "images/melns logo.png";
+    mainLogo.src = "images/main_logo_melns.png";
+  }
+
+  // Add click event listener to theme switch
+  themeSwitch.addEventListener("click", () => {
+    toggleDarkMode();
+  });
+
+  // Add click event listeners to nav links
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      // Update active class on clicked link
+      navLinks.forEach((link) => link.classList.remove("active"));
+      link.classList.add("active");
     });
   });
+});

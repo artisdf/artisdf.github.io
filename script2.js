fbdd0d084a0e4d6e2f07fcf883065ebdebc7396b
artisdf <artisdf18@gmail.com>
2024-06-05 17:21:32 +0300

    Updates to second page

--- script2.js
+++ script2.js
@@ -2,23 +2,37 @@ document.addEventListener("DOMContentLoaded", () => {
   const navLinks = document.querySelectorAll(".nav-link");
   const themeSwitch = document.querySelector(".theme-switch");
   const logo = document.querySelector(".logo");
-  const mainLogo = document.getElementById("main-logo");
   const themeIcon = document.getElementById("theme-icon");
+  const arrow1 = document.getElementById("arrow1");
+  const arrow2 = document.getElementById("arrow2");
+  const arrow3 = document.getElementById("arrow3");
+  const arrow4 = document.getElementById("arrow4");
+  const arrow5 = document.getElementById("arrow5");
+  const arrow6 = document.getElementById("arrow6");
   const body = document.body;
-
   // Function to toggle dark mode
   const toggleDarkMode = () => {
     body.classList.toggle("dark-mode");
     if (body.classList.contains("dark-mode")) {
       themeIcon.src = "images/dark-mode.png";
       logo.src = "images/melns-logo.png";
-      mainLogo.src = "images/main_logo_melns.png";
+      arrow1.src = "images/bulta-melna-2.png";
+      arrow2.src = "images/bulta-melna-2.png";
+      arrow3.src = "images/bulta-melna-2.png";
+      arrow4.src = "images/bulta-melna-2.png";
+      arrow5.src = "images/bulta-melna-2.png";
+      arrow6.src = "images/bulta-melna-2.png";
       // Save dark mode state to Session Storage
       sessionStorage.setItem("darkMode", "true");
     } else {
       themeIcon.src = "images/light-mode.png";
       logo.src = "images/balts-logo.png";
-      mainLogo.src = "images/main_logo_balts.png";
+      arrow1.src = "images/bulta-balta-2.png";
+      arrow2.src = "images/bulta-balta-2.png";
+      arrow3.src = "images/bulta-balta-2.png";
+      arrow4.src = "images/bulta-balta-2.png";
+      arrow5.src = "images/bulta-balta-2.png";
+      arrow6.src = "images/bulta-balta-2.png";
       // Remove dark mode state from Session Storage
       sessionStorage.removeItem("darkMode");
     }
@@ -30,7 +44,12 @@ document.addEventListener("DOMContentLoaded", () => {
     body.classList.add("dark-mode");
     themeIcon.src = "images/dark-mode.png";
     logo.src = "images/melns-logo.png";
-    mainLogo.src = "images/main_logo_melns.png";
+    arrow1.src = "images/bulta-melna-2.png";
+    arrow2.src = "images/bulta-melna-2.png";
+    arrow3.src = "images/bulta-melna-2.png";
+    arrow4.src = "images/bulta-melna-2.png";
+    arrow5.src = "images/bulta-melna-2.png";
+    arrow6.src = "images/bulta-melna-2.png";
   }
 
   // Add click event listener to theme switch
